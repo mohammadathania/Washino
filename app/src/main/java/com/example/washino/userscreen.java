e0f332547900c07b7bbac9a259138946abd0b88d
mohammadathania <mohammad.athania@somaiya.edu>
2020-05-14 23:10:19 +0530

    Third commit

--- app/src/main/java/com/example/washino/userscreen.java
+++ app/src/main/java/com/example/washino/userscreen.java
@@ -2,22 +2,34 @@ package com.example.washino;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class userscreen extends AppCompatActivity {
+    Button createaccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_userscreen);
+        setContentView(R.layout.activity_userscreen);
+        createaccount=findViewById(R.id.create_account);
+        createaccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), Login.class));
+
+            }
+        });
 
     }
-    public void btnClicked(View view) {
 
-        Toast.makeText(this, "Button Was Clicked", Toast.LENGTH_SHORT).show();
-    }
+
+
+
 }
